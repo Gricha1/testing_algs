a66b591d1629716e096b803da810b1423dbd1746
Gricha1 <trololoeeev@mail.ru>
2024-07-01 22:47:00 +0300

    gregory: add exps

--- train_hrac_safety_1.sh
+++ train_hrac_safety_1.sh
@@ -4,7 +4,8 @@
 #python main.py --train_safe_model --eval_freq 100000 --controller_safe_model --man_rew_scale 0.1 --goal_loss_coeff 20 --max_timesteps 1500000 --wandb_postfix ", testing_safety_subgoal, safety_loss_coef=2000"
 #python main.py --validation_without_image --random_start_pose --eval_freq 30000 --safety_subgoals --testing_safety_subgoal --train_safe_model --controller_safe_model --man_rew_scale 0.1 --goal_loss_coeff 20 --safety_loss_coef 400 --max_timesteps 1800000 --wandb_postfix ""
 
-python main.py --modelbased_safety --modelfree_safety \
+python main.py --load_adj_net --load --loaded_exp_num 58\
+               --modelbased_safety --modelfree_safety \
                --coef_safety_modelbased 0.8 --coef_safety_modelfree 0.2 \
                --subgoal_grad_clip 600 \
                --validation_without_image --eval_freq 30000 \
@@ -15,7 +16,5 @@ python main.py --modelbased_safety --modelfree_safety \
                --adj_loss_coef 0.0 \
                --man_rew_scale 0.1 --goal_loss_coeff 20.0 --safety_loss_coef 3000 \
                --max_timesteps 1500000 \
-               --wandb_postfix "" \
-               --not_use_wandb \
-               --tensorboard_descript "safety_mb=0.8, safety_mf=0.2, safety=3000, grad=600"
+               --wandb_postfix "" 
 
