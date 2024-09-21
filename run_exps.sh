821d8157997daa4a3ab0c75c42df011186f981a1
Gricha1 <trololoeeev@mail.ru>
2024-09-21 18:15:40 +0300

    gregory: update docker file & add exps folder

--- run_exps.sh
+++
@@ -1,5 +0,0 @@
-
-python run.py --robot point --level 1 --dir data/pg1 -n test-rce -o rce --epoch 80 --c config_1.yml
-
-
-python run.py --robot point --level 1 --dir data/pg1 -n test-rce -o rce --epoch 80 --c config.yml
