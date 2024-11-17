8d0a24801547845e4071c6d370f23bd8eb1ca6d2
Gricha1 <trololoeeev@mail.ru>
2024-11-17 19:59:34 +0300

    gregory: add td3 img safety c shape exps

---
+++ exps/td3/train_safe_ant_maze_c_td3_safe.sh
@@ -0,0 +1,22 @@
+if [ -z "$1" ]; then
+    seed=344
+else
+    seed=$1
+fi
+
+cd ../..
+python main.py --env_name SafeAntMazeC \
+               --seed $seed \
+               --train_only_td3 \
+               --world_model \
+               --cost_model \
+               --cm_pretrain \
+               --cost_memmory \
+               --controller_imagination_safety_loss \
+               --controller_safety_coef 6 \
+               --validation_without_image --eval_freq 30000 \
+               --max_timesteps 4000000 \
+               --wandb_postfix "" \
+               --not_use_wandb \
+               --random_start_pose
+
