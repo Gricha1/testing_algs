8e5cf47720122120ca2a981abd3311f2f448f29f
Gricha1 <trololoeeev@mail.ru>
2024-11-24 15:56:03 +0300

    gregory: add hrac safe train on s shape 2

---
+++ exps/hrac_safety/train_safe_ant_maze_s_hrac_safety.sh
@@ -0,0 +1,23 @@
+if [ -z "$1" ]; then
+    seed=2
+else
+    seed=$1
+fi
+
+cd ../..
+python main.py --seed $seed \
+               --modelfree_safety \
+               --env_name SafeAntMazeS \
+               --controller_imagination_safety_loss \
+               --validation_without_image --eval_freq 30000 \
+               --random_start_pose \
+               --world_model \
+               --cost_memmory \
+               --cost_model \
+               --man_rew_scale 0.1 --goal_loss_coeff 20.0 \
+               --coef_safety_modelfree 800 \
+               --controller_safety_coef 6 \
+               --max_timesteps 4000000 \
+               --wandb_postfix "" \
+               --not_use_wandb
+
