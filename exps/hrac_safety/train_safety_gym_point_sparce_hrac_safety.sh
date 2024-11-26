4233cc47a014944802c66030115054db8bcd3555
Gricha1 <trololoeeev@mail.ru>
2024-11-26 18:51:00 +0300

    gregory: add safety gym sparce exp for hrac_safe

---
+++ exps/hrac_safety/train_safety_gym_point_sparce_hrac_safety.sh
@@ -0,0 +1,40 @@
+if [ -z "$1" ]; then
+    seed=344
+else
+    seed=$1
+fi
+
+
+
+cd ../..
+python main.py --domain_name Safexp \
+               --task_name PointGoal1 \
+               --env_name SafeGym \
+               --action_repeat 2 \
+               --goal_conditioned \
+               --vector_env \
+               --sparce_reward \
+               --seed $seed \
+               --world_model \
+               --modelfree_safety \
+               --controller_imagination_safety_loss \
+               --controller_grad_clip 0 \
+               --validation_without_image --eval_freq 1000 \
+               --cost_model \
+               --cm_frame_stack_num 1 \
+               --cm_pretrain \
+               --cost_model_batch_size 512 \
+               --man_rew_scale 100.0 \
+               --goal_loss_coeff 20.0 \
+               --coef_safety_modelfree 10.0 \
+               --controller_safety_coef 0.001 \
+               --max_timesteps 4000000 \
+               --img_horizon 10 \
+               --manager_propose_freq 10 \
+               --train_manager_freq 5 \
+               --wm_pretrain \
+               --wm_pretrain_epoches 100 \
+               --wm_n_initial_exploration_steps 30000 \
+               --wandb_postfix "" \
+               --not_use_wandb
+
