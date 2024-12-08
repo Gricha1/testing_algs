6d2cfe1a3c8b768d1e41290adaccad68716c50ab
Gricha1 <trololoeeev@mail.ru>
2024-12-08 19:21:19 +0300

    gregory: fix world model training

--- hrac/world_models.py
+++ hrac/world_models.py
@@ -297,7 +297,7 @@ class EnsembleDynamicsModel():
             #print('epoch: {}, train mse losses: {}'.format(epoch, np.mean(train_mse_losses,axis=0)))
             #print('epoch: {}, holdout mse losses: {}'.format(epoch, holdout_mse_losses))
 
-            return epoch, np.mean(np.mean(train_mse_losses,axis=0))
+        return epoch, np.mean(np.mean(train_mse_losses,axis=0))
 
     def _save_best(self, epoch, holdout_losses):
         updated = False
